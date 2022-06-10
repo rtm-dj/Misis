91067d6794f163fd518a4a5c36c8504e11b533dd
rtm-dj <artem@yugaev.ru>
2022-06-10 18:08:45 +0300

    все сделаль

--- project/script.js
+++ project/script.js
@@ -25,8 +25,15 @@ function reminder(){
 function valide(){
     var name = document.getElementById("name").value;
     var password = document.getElementById("password").value;
+    
     var reg_name = /^[a-zа-я]+$/i;
     var pass_name = /^[a-z0-9]{5,100}/i;
     
+    if (reg_name.test(name) == false){
+        alert('ФИО должно содержать только буквы!')
+    }
+    if (pass_name.test(password) == false){
+        alert('Пароль должен содержать английский алфавит, цифры. Длина 5-100 символов')
+    }
 }
 document.querySelector(".button").addEventListener("click", valide)
